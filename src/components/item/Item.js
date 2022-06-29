6dc1ef8c1ab36451618fbc05f709d5bbab967616
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-06-29 22:29:07 +0300

    project end

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,1 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Item.css";
+const Item = ({ card }) => {
+  const [showLogo, setShowLogo] = useState(true);
+
+  const { name, img, options } = card;
+
+  return (
+    <div
+      className="card"
+      onClick={() => {
+        setShowLogo(!showLogo);
+      }}
+    >
+      {showLogo ? (
+        <div>
+          <img src={img} alt="" className="card-logo" />
+          <h3 className="card-title">{name} </h3>
+        </div>
+      ) : (
+        <ul className="list">
+          {options.map((element, index) => {
+            return <li key={index}>{element} </li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Item;
